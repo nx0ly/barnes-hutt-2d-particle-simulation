9d6e065a3f8d832b6c37ceff5f0cf3f7a713e394
nx0ly <n9d8sa@gmail.com>
2025-09-09 20:35:07 -0400

    working prototype

---
+++ rust/src/main.rs
@@ -0,0 +1,137 @@
+use quadtree::{BHQuadtree, Point, Vec2, WeightedPoint};
+use rand::Rng;
+use three_d::*;
+
+#[derive(Clone)]
+struct Particle {
+    pub pos: Vec2,
+    pub vel: Vec2,
+    pub mass: f32,
+    //pub x: f32,
+    //pub y: f32,
+    //pub color: [u8; 4],
+    //pub size: f32,
+}
+type Particles = Vec<Particle>; // ok listen maybe not best practices but does it matter?
+
+impl Particle {
+    pub fn new(x: f32, y: f32) -> Self {
+        Particle {
+            pos: Vec2::new(x, y),
+            vel: Vec2::new(0., 0.),
+            mass: 5., //color: [255, 0, 0, 255],
+                      //size: 5.,
+        }
+    }
+}
+
+impl quadtree::Point for Particle {
+    fn point(&self) -> Vec2 {
+        self.pos
+    }
+}
+
+impl From<Particle> for WeightedPoint {
+    fn from(value: Particle) -> Self {
+        WeightedPoint {
+            pos: value.pos,
+            mass: value.mass,
+        }
+    }
+}
+
+fn main() {
+    let (window_width, window_height) = (1280., 720.);
+    let window = Window::new(WindowSettings {
+        title: "barnes hutt particle simulation".to_string(),
+        min_size: (window_width as u32, window_height as u32),
+        ..Default::default()
+    })
+    .unwrap();
+    let (context, window_viewport, window_dpi) =
+        (window.gl(), window.viewport(), window.device_pixel_ratio());
+    let mut rng = rand::rng();
+    let mut camera = Camera::new_2d(window_viewport);
+    let mut control = Control2D::new(0.5, 100.);
+    let mut bh_quad = BHQuadtree::new(0.5);
+
+    let mut particles: Vec<Particle> = Vec::new();
+    for _ in 0..100 {
+        let particle = Particle::new(
+            rng.random_range(-3000.0..window_width) + window_width / 2.,
+            rng.random_range(-3000.0..window_height) + window_height / 2.,
+        );
+        particles.push(particle);
+    }
+
+    let particle_cpu_mat = CpuMaterial {
+        albedo: Srgba::RED,
+        ..Default::default()
+    };
+    let mut particle_mesh = Gm::new(
+        InstancedMesh::new(&context, &Instances::default(), &CpuMesh::circle(12)),
+        ColorMaterial::new(&context, &particle_cpu_mat),
+    );
+
+    window.render_loop(move |mut frame_input| {
+        camera.set_viewport(frame_input.viewport);
+        control.handle_events(&mut camera, &mut frame_input.events, window_dpi);
+        update_simulation(&mut bh_quad, &mut particles);
+
+        /*for (mesh, p) in particle_meshes.iter_mut().zip(particles.iter()) {
+            mesh.set_transformation(Mat4::from_translation(vec3(
+                p.pos.x - window_width / 2.0,
+                p.pos.y - window_height / 2.0,
+                0.0,
+            )));
+        }*/
+
+        let transforms: Vec<Mat4> = particles
+            .iter()
+            .map(|p| {
+                Mat4::from_translation(vec3(
+                    p.pos.x - window_width / 2.0,
+                    p.pos.y - window_height / 2.0,
+                    0.0,
+                )) * Mat4::from_scale(5.0)
+            })
+            .collect();
+
+        particle_mesh.set_instances(&Instances {
+            transformations: transforms,
+            ..Default::default()
+        });
+
+        frame_input
+            .screen()
+            .clear(ClearState::color_and_depth(0., 0., 0., 1., 1.))
+            .render(&camera, std::iter::once(&mut particle_mesh), &[]);
+
+        FrameOutput::default()
+    });
+}
+
+fn update_simulation(bh_quad: &mut BHQuadtree, particles: &mut Vec<Particle>) {
+    bh_quad.build(particles.iter().cloned().map(Into::into).collect(), 4);
+
+    for p in particles {
+        let target = p.pos;
+
+        let total_force: Vec2 = bh_quad.accumulate(target, |wp: WeightedPoint| {
+            if wp.pos == target {
+                return Vec2::new(0.0, 0.0);
+            }
+
+            let dir = target - wp.pos;
+            let dist_sq = dir.length().powi(2) + 0.005;
+            let strength = wp.mass / dist_sq;
+            dir.normalize() * strength
+        });
+
+        // F = m*a
+        // hah physics finally coming in clutch
+        let accel = total_force / p.mass;
+        p.vel += accel * 0.5;
+        p.pos += p.vel * 0.5;
+    }
+}
